e404376d9eb61ab6393697b6b3e186bab2ae4d5c
Oleg <o.kuzicevs@gmail.com>
2022-10-23 15:59:31 +0300

    initial

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "products list";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "single product";
+        }
+    }
+}
